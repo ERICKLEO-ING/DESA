69320fc365fad0d2f754a70cf7da89b4def164b8
Erick <edelacruz@infomatica.pe>
2021-06-26 01:40:59 -0500

    limpieza

--- ERICK/InfoService/bin/Debug/instalar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
-installutil.exe "D:\DESARROLLO\ERICK\InfoService\bin\Debug\InfoService.exe"
-
-if ERRORLEVEL 1 goto error
-exit
-:error
-echo There was a problem
-pause
